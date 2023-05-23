8e0304a7f8fd559a672f22a839b21c61d26e6b2f
Heon Soo Park <hspark220@gmail.com>
2023-05-23 13:30:52 -0400

    Add social icons

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -1,7 +1,11 @@
+import { SocialIcon } from 'react-social-icons';
+
 const Contact = () => {
     return (
         <div className="contact">
             contact
+            <br></br>
+            <SocialIcon url="https://github.com/hspark220" />
         </div>
     )
 }
